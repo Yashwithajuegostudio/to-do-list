b2a0b342492f4f1f1f14d528f1e77de7672495a2
Yashwitha <yashwitha@juegostudioz.com>
2022-11-08 17:46:17 +0530

    BUGFIX:Feedback changes are updated

--- src/components/ToDoListContainer/ToDoListContainer.jsx
+++ src/components/ToDoListContainer/ToDoListContainer.jsx
@@ -1,38 +1,14 @@
-import React, { useState } from "react";
 import { title } from "../../utils/constant";
 import Button from "../Button/Button";
-import PopUP from "../PopUp/PopUp";
 
 import styles from "./ToDoListContainer.module.css";
 
 function ToDoListContainer() {
-  const [visibility, setVisibility] = useState(false);
-
-  const popupCloseHandler = (e) => {
-    setVisibility(e);
-  };
   return (
     <div className={styles.todo_list_container}>
       <div className={styles.todo_buttons}>
-        <Button
-          clickHandler={(e) => setVisibility(!visibility)}
-          title={title.EditBtnTitle}
-        ></Button>
-        <Button
-          clickHandler={(e) => setVisibility(!visibility)}
-          title={title.DeleteBtnTitle}
-        ></Button>
-
-        <PopUP
-          onClose={popupCloseHandler}
-          show={visibility}
-          title="Are you sure want to save?"
-        >
-          <div className={styles.todo_buttons}>
-            <Button title={title.saveBtnTitle}></Button>
-            <Button title={title.cancelBtnTitle}></Button>
-          </div>
-        </PopUP>
+        <Button title={title.EditBtnTitle}></Button>
+        <Button title={title.DeleteBtnTitle}></Button>
       </div>
     </div>
   );
