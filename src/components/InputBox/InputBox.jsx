e9d852fe5752b10178380897119683f9edabf2ce
Yashwithajuegostudio <yashwitha.juegostudio@gmail.com>
2022-11-09 07:54:58 +0530

    FEAT:Input box component updated

--- src/components/InputBox/InputBox.jsx
+++ src/components/InputBox/InputBox.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import styles from "./InputBox.module.css";
 
-function InputBox({ handleOnchange, type, value }) {
+function InputBox({ handleOnchange, type, value, id, name }) {
   return (
     <input
       className={styles.input_box}
       onChange={handleOnchange}
       type={type}
       value={value}
+      id={id}
+      name={name}
     ></input>
   );
 }
