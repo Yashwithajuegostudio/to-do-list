2f80bba350b702e18259dbfddbf87cf4f9cd5998
Yashwitha <yashwitha@juegostudioz.com>
2022-11-16 15:23:21 +0530

    BUGFIX:Feedback changes are updated

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import "./App.css";
 import ToDoList from "./pages/ToDoList/ToDoList";
 import Login from "./pages/Login/Login";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Path } from "./utils/constant";
+import { PATH } from "./utils/constant";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path={Path.todoListPath} element={<ToDoList />} />
+        <Route path={PATH.todoListPath} element={<ToDoList />} />
         <Route path="/" element={<Login />} />
       </Routes>
     </Router>
